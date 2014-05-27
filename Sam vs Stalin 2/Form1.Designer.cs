6b589f319a7e1a76aca095d05add5f6cb47389b1
Nick <nickmax123@gmail.com>
2014-05-26 22:38:28 -0400

    initial commit

---
+++ Sam vs Stalin 2/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿using System;
+namespace Sam_vs_Stalin_2
+{
+    partial class SinglePlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.leonidas = new System.Windows.Forms.PictureBox();
+            this.xerxes = new System.Windows.Forms.PictureBox();
+            this.spear = new System.Windows.Forms.PictureBox();
+            this.fez = new System.Windows.Forms.PictureBox();
+            this.lblHealth = new System.Windows.Forms.Label();
+            this.leonidasHealth = new System.Windows.Forms.ProgressBar();
+            this.xerxesHealth = new System.Windows.Forms.ProgressBar();
+            this.tmrLeoUp = new System.Windows.Forms.Timer(this.components);
+            this.tmrLeoDown = new System.Windows.Forms.Timer(this.components);
+            this.tmrLeoLeft = new System.Windows.Forms.Timer(this.components);
+            this.tmrLeoRight = new System.Windows.Forms.Timer(this.components);
+            this.tmrSpear = new System.Windows.Forms.Timer(this.components);
+            this.tmrXerxes = new System.Windows.Forms.Timer(this.components);
+            this.tmrXerxesMovement = new System.Windows.Forms.Timer(this.components);
+            this.tmrFez = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.leonidas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xerxes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fez)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // leonidas
+            // 
+            this.leonidas.BackColor = System.Drawing.Color.Transparent;
+            this.leonidas.BackgroundImage = global::Sam_vs_Stalin_2.Properties.Resources.leonidas;
+            this.leonidas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.leonidas.Location = new System.Drawing.Point(12, 421);
+            this.leonidas.Name = "leonidas";
+            this.leonidas.Size = new System.Drawing.Size(137, 100);
+            this.leonidas.TabIndex = 0;
+            this.leonidas.TabStop = false;
+            // 
+            // xerxes
+            // 
+            this.xerxes.BackColor = System.Drawing.Color.Transparent;
+            this.xerxes.BackgroundImage = global::Sam_vs_Stalin_2.Properties.Resources.xerxes;
+            this.xerxes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.xerxes.Location = new System.Drawing.Point(1380, 421);
+            this.xerxes.Name = "xerxes";
+            this.xerxes.Size = new System.Drawing.Size(137, 100);
+            this.xerxes.TabIndex = 1;
+            this.xerxes.TabStop = false;
+            // 
+            // spear
+            // 
+            this.spear.BackColor = System.Drawing.Color.Transparent;
+            this.spear.BackgroundImage = global::Sam_vs_Stalin_2.Properties.Resources.spear;
+            this.spear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.spear.Location = new System.Drawing.Point(155, 456);
+            this.spear.Name = "spear";
+            this.spear.Size = new System.Drawing.Size(77, 33);
+            this.spear.TabIndex = 2;
+            this.spear.TabStop = false;
+            this.spear.Tag = "Leonidas";
+            this.spear.Visible = false;
+            // 
+            // fez
+            // 
+            this.fez.BackColor = System.Drawing.Color.Transparent;
+            this.fez.BackgroundImage = global::Sam_vs_Stalin_2.Properties.Resources.fez;
+            this.fez.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.fez.Location = new System.Drawing.Point(1324, 456);
+            this.fez.Name = "fez";
+            this.fez.Size = new System.Drawing.Size(50, 33);
+            this.fez.TabIndex = 3;
+            this.fez.TabStop = false;
+            this.fez.Tag = "Xerxes";
+            this.fez.Visible = false;
+            // 
+            // lblHealth
+            // 
+            this.lblHealth.AutoSize = true;
+            this.lblHealth.BackColor = System.Drawing.Color.Aqua;
+            this.lblHealth.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHealth.Location = new System.Drawing.Point(0, -3);
+            this.lblHealth.MaximumSize = new System.Drawing.Size(1545, 25);
+            this.lblHealth.MinimumSize = new System.Drawing.Size(1530, 25);
+            this.lblHealth.Name = "lblHealth";
+            this.lblHealth.Size = new System.Drawing.Size(1530, 25);
+            this.lblHealth.TabIndex = 4;
+            this.lblHealth.Text = "HEALTH";
+            this.lblHealth.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // leonidasHealth
+            // 
+            this.leonidasHealth.Location = new System.Drawing.Point(244, -1);
+            this.leonidasHealth.Name = "leonidasHealth";
+            this.leonidasHealth.Size = new System.Drawing.Size(185, 23);
+            this.leonidasHealth.TabIndex = 5;
+            this.leonidasHealth.Value = 100;
+            // 
+            // xerxesHealth
+            // 
+            this.xerxesHealth.Location = new System.Drawing.Point(1087, -1);
+            this.xerxesHealth.Name = "xerxesHealth";
+            this.xerxesHealth.Size = new System.Drawing.Size(185, 23);
+            this.xerxesHealth.TabIndex = 6;
+            this.xerxesHealth.Value = 100;
+            // 
+            // tmrLeoUp
+            // 
+            this.tmrLeoUp.Interval = 1;
+            this.tmrLeoUp.Tick += new System.EventHandler(this.tmrLeoUp_Tick);
+            // 
+            // tmrLeoDown
+            // 
+            this.tmrLeoDown.Interval = 1;
+            this.tmrLeoDown.Tick += new System.EventHandler(this.tmrLeoDown_Tick);
+            // 
+            // tmrLeoLeft
+            // 
+            this.tmrLeoLeft.Interval = 1;
+            this.tmrLeoLeft.Tick += new System.EventHandler(this.tmrLeoLeft_Tick);
+            // 
+            // tmrLeoRight
+            // 
+            this.tmrLeoRight.Interval = 1;
+            this.tmrLeoRight.Tick += new System.EventHandler(this.tmrLeoRight_Tick);
+            // 
+            // tmrSpear
+            // 
+            this.tmrSpear.Interval = 1;
+            this.tmrSpear.Tick += new System.EventHandler(this.tmrSpear_Tick);
+            // 
+            // tmrXerxes
+            // 
+            this.tmrXerxes.Enabled = true;
+            this.tmrXerxes.Interval = 300;
+            this.tmrXerxes.Tick += new System.EventHandler(this.tmrXerxes_Tick);
+            // 
+            // tmrXerxesMovement
+            // 
+            this.tmrXerxesMovement.Interval = 1;
+            this.tmrXerxesMovement.Tick += new System.EventHandler(this.tmrXerxesMovement_Tick);
+            // 
+            // tmrFez
+            // 
+            this.tmrFez.Interval = 1;
+            this.tmrFez.Tick += new System.EventHandler(this.tmrFez_Tick);
+            // 
+            // SinglePlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Sam_vs_Stalin_2.Properties.Resources.greece_map;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1529, 865);
+            this.Controls.Add(this.xerxesHealth);
+            this.Controls.Add(this.leonidasHealth);
+            this.Controls.Add(this.lblHealth);
+            this.Controls.Add(this.fez);
+            this.Controls.Add(this.spear);
+            this.Controls.Add(this.xerxes);
+            this.Controls.Add(this.leonidas);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "SinglePlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sam vs Stalin 2: Leonidas vs Xerxes";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SinglePlayer_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.SinglePlayer_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.leonidas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xerxes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fez)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox leonidas;
+        private System.Windows.Forms.PictureBox xerxes;
+        private System.Windows.Forms.PictureBox spear;
+        private System.Windows.Forms.PictureBox fez;
+        private System.Windows.Forms.Label lblHealth;
+        private System.Windows.Forms.ProgressBar leonidasHealth;
+        private System.Windows.Forms.ProgressBar xerxesHealth;
+        private System.Windows.Forms.Timer tmrLeoUp;
+        private System.Windows.Forms.Timer tmrLeoDown;
+        private System.Windows.Forms.Timer tmrLeoLeft;
+        private System.Windows.Forms.Timer tmrLeoRight;
+        private System.Windows.Forms.Timer tmrSpear;
+        private System.Windows.Forms.Timer tmrXerxes;
+        private System.Windows.Forms.Timer tmrXerxesMovement;
+        private System.Windows.Forms.Timer tmrFez;
+    }
+}
+
---
+++ Sam vs Stalin 2/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Sam_vs_Stalin_2
+{
+    public partial class SinglePlayer : Form
+    {
+
+        Random rnd = new Random();
+        int dir;
+
+        public SinglePlayer()
+        {
+            InitializeComponent();
+            
+        }
+
+        private Boolean weaponFired(System.Windows.Forms.Timer weapon)
+        {
+            if (weapon.Enabled == true)
+                return true;
+            return false;
+        }
+
+        private void SinglePlayer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+                tmrLeoUp.Enabled    = true;
+            if (e.KeyCode == Keys.S)
+                tmrLeoDown.Enabled  = true;
+            if (e.KeyCode == Keys.A)
+                tmrLeoLeft.Enabled  = true;
+            if (e.KeyCode == Keys.D)
+                tmrLeoRight.Enabled = true;
+            if (e.KeyCode == Keys.Space)
+            {
+                if (!weaponFired(tmrSpear))
+                {
+                    spear.Location = new Point(leonidas.Location.X + leonidas.Width - 20, leonidas.Location.Y + 25);
+                    spear.Visible    = true;
+                    tmrSpear.Enabled = true;
+                }
+                
+            }
+
+        }
+   
+        private void SinglePlayer_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+                tmrLeoUp.Enabled    = false;
+            else if (e.KeyCode == Keys.S)
+                tmrLeoDown.Enabled  = false;
+            else if (e.KeyCode == Keys.A)
+                tmrLeoLeft.Enabled  = false;
+            else if (e.KeyCode == Keys.D)
+                tmrLeoRight.Enabled = false;
+
+        }
+
+        private void tmrLeoUp_Tick(object sender, EventArgs e)
+        {
+            outOfBounds(leonidas);
+            leonidas.Top -= 4;
+
+        }
+
+        private void tmrLeoDown_Tick(object sender, EventArgs e)
+        {
+            outOfBounds(leonidas);
+            leonidas.Top += 4;
+        }
+
+        private void tmrLeoLeft_Tick(object sender, EventArgs e)
+        {
+            outOfBounds(leonidas);
+            leonidas.Left -= 4;
+        }
+
+        private void tmrLeoRight_Tick(object sender, EventArgs e)
+        {
+            outOfBounds(leonidas);
+            leonidas.Left += 4;
+        }
+
+        private void outOfBounds(PictureBox player)
+        {
+            int x = player.Left;
+            int y = player.Top;
+
+            if (x < 0) 
+                player.Left = 0;
+
+            if ((player.Name == "leonidas") && x > this.Size.Width / 2 - player.Width) 
+                player.Left = this.Size.Width / 2 - player.Width;
+            if ((player.Name == "xerxes") && x < this.Size.Width / 2)
+                player.Left = this.Size.Width / 2;
+            if ((player.Name == "xerxes") && player.Right > this.Size.Width)
+                player.Left = this.Size.Width - player.Width;
+            if (y < 17) 
+                player.Top = 17;
+            if (y > this.Size.Height - player.Height-35) 
+                player.Top = this.Size.Height - player.Height-35;
+        }
+
+        private void disableTimers()
+        {
+            tmrLeoUp.Enabled          = false;
+            tmrLeoDown.Enabled        = false;
+            tmrLeoLeft.Enabled        = false;
+            tmrLeoRight.Enabled       = false;
+            tmrSpear.Enabled          = false;
+            tmrXerxes.Enabled         = false;
+            tmrXerxesMovement.Enabled = false;
+            tmrFez.Enabled            = false;
+
+
+        }
+
+        private void tmrSpear_Tick(object sender, EventArgs e)
+        {
+            collision(xerxes, spear, xerxesHealth, tmrSpear);
+            spear.Left += 40;
+        }
+
+        private void collision(PictureBox player, PictureBox weapon, ProgressBar health, System.Windows.Forms.Timer timer)
+        {
+           
+            if (weapon.Left > this.Size.Width || weapon.Right < 0)
+            {
+                timer.Enabled  = false;
+                weapon.Visible = false;
+            }
+            else if (((weapon.Name == "spear") && (weapon.Right > player.Left + 15 ))
+                  || ((weapon.Name == "fez")   && (weapon.Left < player.Right - 15))
+                  &&  (weapon.Bottom > player.Top + 5) 
+                  &&  (weapon.Top < player.Bottom - 5))
+            {
+                timer.Enabled  = false;
+                weapon.Visible = false;
+                health.Value -= 25;
+                if (health.Value == 0)
+                {
+                    disableTimers();
+                    MessageBox.Show(weapon.Tag + " wins!");
+                }
+            }
+            
+        }
+
+        private void tmrXerxes_Tick(object sender, EventArgs e)
+        {
+            dir = rnd.Next(0, 4);
+            tmrXerxesMovement.Enabled = true;
+            if (!weaponFired(tmrFez))
+            {
+                fez.Location = new Point(xerxes.Location.X, xerxes.Location.Y + 20);
+                fez.Visible = true;
+                tmrFez.Enabled = true;
+            }
+        }
+
+        private void tmrXerxesMovement_Tick(object sender, EventArgs e)
+        {
+            // Up
+            if (dir == 0)
+            {
+                outOfBounds(xerxes);
+                xerxes.Top -= 5;
+            }
+            else if (dir == 1)
+            {
+                outOfBounds(xerxes);
+                xerxes.Top += 5;
+            }
+            else if (dir == 2)
+            {
+                outOfBounds(xerxes);
+                xerxes.Left -= 5;
+            }
+            else if (dir == 3)
+            {
+                outOfBounds(xerxes);
+                xerxes.Left += 5;
+            }
+        }
+
+        private void tmrFez_Tick(object sender, EventArgs e)
+        {
+            collision(leonidas, fez, leonidasHealth, tmrFez);
+            fez.Left -= 20;
+        }
+
+        
+    }
+}
